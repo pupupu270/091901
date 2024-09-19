6bae33c1bb3ecdf1d86e05b1c0ff137d6bfcb189
pupupu270 <lycee0@yahoo.co.jp>
2024-09-19 11:33:28 +0900

    Update httpServer.js

--- httpServer.js
+++ httpServer.js
@@ -3,7 +3,7 @@
 let http = require("http");
 let fs = require("fs");
 
-let server = http.createServer((req,res)=>{
+http.createServer((req,res)=>{
 res.writeHead(200,{"Content-Type":"text/html;charset = utf-8"});
 res.write("<html><head></head><body>test</body></html>");
 res.end();
